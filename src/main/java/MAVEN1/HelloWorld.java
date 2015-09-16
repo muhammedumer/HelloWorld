7bc478c3cb43bcc7fbf69a4e1ef1c2b5252043ea
muhammedumer <imuhammed.umer@gmail.com>
2015-09-16 12:56:38 +0800

    Initial commit

---
+++ src/main/java/MAVEN1/HelloWorld.java
@@ -0,0 +1,13 @@
+package MAVEN1;
+
+/**
+ * Created by user on 16/09/2015.
+ */
+public class HelloWorld {
+    public static void main (String [] args)
+    {
+
+        System.out.println("Hello World");
+    }
+}
+
